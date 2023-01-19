9c077c9d71bc8f56dd5c7291e488b5ac3c1daa21
kunalll1 <kunalkumarjha97@gmail.com>
2023-01-19 22:05:05 +0530

    now able to add multiple number to check it's Even or Odd

--- is-no-even-or-odd.js
+++ is-no-even-or-odd.js
@@ -7,4 +7,18 @@ function checkEvenAndOdd(num) {
   }
 }
 
-checkEvenAndOdd(5);
+checkEvenAndOdd(4);
+
+//TODO: Adding little bit complexity to this program :::: Now what i want we have a array on number and we want to check if its EVEN or ODD
+
+function arrOfNumber(numb) {
+  for (let listOfNum in numb) {
+    if (listOfNum % 2 == 0) {
+      console.log(`${listOfNum} is Even`);
+    } else {
+      console.log(`${listOfNum} is Odd`);
+    }
+  }
+}
+
+arrOfNumber([2, 3, 4, 5, 5, 7, 8, 9, 19]);
