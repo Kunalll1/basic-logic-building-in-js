d691f55d2edb7b516c0c6bf6767571532d58d793
kunalll1 <kunalkumarjha97@gmail.com>
2023-02-06 22:49:42 +0530

    solved 1st qustion of ForEach callback function problem

---
+++ prec-foreach-callback.js
@@ -0,0 +1,14 @@
+// ======================================================
+// BASIC QUESTION
+// ======================================================
+//[Qus-1] Write a function that takes an array of numbers as input and logs each number to the console using forEach().
+
+function numberArr(num) {
+  num.forEach((numArr) => {
+    // console.log(numArr);
+  });
+}
+
+numberArr([100, 10, 20, 50, 30]);
+
+// ====================================================================
